f0dfc26f93ef3679deeb13b27528da58de969027
Yefry Moraga <jeffamed@gmail.com>
2026-03-24 18:13:00 -0600

    sidebar and header

---
+++ src/components/layout/AppSidebar.vue
@@ -0,0 +1,44 @@
+<template>
+  <aside
+    :class="[
+      'fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-99999 border-r border-gray-200',
+      {
+        'lg:w-[290px]': isExpanded || isMobileOpen || isHovered,
+        'lg:w-[90px]': !isExpanded && !isHovered,
+        'translate-x-0 w-[290px]': isMobileOpen,
+        '-translate-x-full': !isMobileOpen,
+        'lg:translate-x-0': true,
+      },
+    ]"
+    @mouseenter="!isExpanded && (isHovered = true)"
+    @mouseleave="isHovered = false"
+  >
+    <div :class="['py-8 flex', !isExpanded && !isHovered ? 'lg:justify-center' : 'justify-start']">
+      <router-link to="/">
+        <img
+          v-if="isExpanded || isHovered || isMobileOpen"
+          class="dark:hidden"
+          src="/images/logo/logo.svg"
+          alt="Logo"
+          width="150"
+          height="40"
+        />
+        <img
+          v-if="isExpanded || isHovered || isMobileOpen"
+          class="hidden dark:block"
+          src="/images/logo/logo-dark.svg"
+          alt="Logo"
+          width="150"
+          height="40"
+        />
+        <img v-else src="/images/logo/logo-icon.svg" alt="Logo" width="32" height="32" />
+      </router-link>
+    </div>
+  </aside>
+</template>
+<script setup lang="ts">
+import { useSidebar } from '@/composables/useSidebar'
+
+const { isExpanded, isMobileOpen, isHovered } = useSidebar()
+</script>
+<style scoped></style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import AdminLayout from '@/components/layout/AdminLayout.vue'
+</script>
+
+<template>
+  <admin-layout />
+</template>
+
+<style scoped></style>
