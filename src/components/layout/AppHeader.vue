e170e07dcb794e0b9fe2efc3b4215612ea4eecb7
Yefry Moraga <jeffamed@gmail.com>
2026-03-26 05:26:34 -0600

    estructura inicial

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,11 +1,9 @@
 <template>
   <header
-    class="sticky top-0 flex w-full bg-white border-gray-200 z-99999 dark:border-gray-800 dark:bg-gray-900 lg:border-b"
-  >
+    class="sticky top-0 flex w-full bg-white border-gray-200 z-99999 dark:border-gray-800 dark:bg-gray-900 lg:border-b">
     <div class="flex flex-col items-center justify-between grow lg:flex-row lg:px-6">
       <div
-        class="flex items-center justify-between w-full gap-2 px-3 py-3 border-b border-gray-200 dark:border-gray-800 sm:gap-4 lg:justify-normal lg:border-b-0 lg:px-0 lg:py-4"
-      >
+        class="flex items-center justify-between w-full gap-2 px-3 py-3 border-b border-gray-200 dark:border-gray-800 sm:gap-4 lg:justify-normal lg:border-b-0 lg:px-0 lg:py-4">
         <button
           @click="handleToggle"
           class="flex items-center justify-center w-10 h-10 text-gray-500 border-gray-200 rounded-lg z-99999 dark:border-gray-800 dark:text-gray-400 lg:h-11 lg:w-11 lg:border"
@@ -67,14 +65,14 @@
           </svg>
         </button>
       </div>
-
       <div
         :class="[isApplicationMenuOpen ? 'flex' : 'hidden']"
-        class="items-center justify-between w-full gap-4 px-5 py-4 shadow-theme-md lg:flex lg:justify-end lg:px-0 lg:shadow-none"
-      >
+        class="items-center justify-between w-full gap-4 px-5 py-4 shadow-theme-md lg:flex lg:justify-end lg:px-0 lg:shadow-none">
         <div class="flex items-center gap-2 2xsm:gap-3">
           <ThemeToggler />
+          <NotificationMenu />
         </div>
+        <UserMenu/>
       </div>
     </div>
   </header>
@@ -84,6 +82,8 @@
 import { ref } from 'vue'
 import { useSidebar } from '@/composables/useSidebar'
 import ThemeToggler from '../common/ThemeToggler.vue'
+import NotificationMenu from '@/components/layout/header/NotificationMenu.vue'
+import UserMenu from '@/components/layout/header/UserMenu.vue'
 
 const { toggleSidebar, toggleMobileSidebar, isMobileOpen } = useSidebar()
 
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,11 +1,10 @@
 <template>
   <admin-layout>
     <div class="grid grid-cols-12 gap-4 md:gap-6">
-      <h1>Dashboard</h1>
+      <h1>Productos</h1>
     </div>
   </admin-layout>
 </template>
-
 <script setup lang="ts">
 import AdminLayout from '@/components/layout/AdminLayout.vue'
 </script>
